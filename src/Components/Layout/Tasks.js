5a55bb074f26866c66d2d4386d0ef5f21bdd734e
Aniket Chaudhary <chaudharyaniket16@gmail.com>
2020-01-04 23:08:41 +0530

    add

--- src/Components/Layout/Tasks.js
+++ src/Components/Layout/Tasks.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { withRouter } from "react-router-dom";
 import Navbar from "./Navbar";
-import TaskContent from "./TaskContent";
+import TaskContent from "./TaskContent
 
 const TaskComponent = styled.div`
   min-width: 76vw;
